9d094a0124148a685d077e4d1020b4a832bba44b
ttverin <67685914+ttverin@users.noreply.github.com>
2020-09-09 16:34:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+
+
+
 function IsPalindrome(palindrome) {
   console.log(palindrome.palindrome)
-  var regEx = new RegExp(/\W+/gi);  //remove non alphabetic characters and blanks
+  console.log({palindrome});
+  var regEx = new RegExp();  //remove non alphabetic characters and blanks ** add later
   var palindromeForward = palindrome.palindrome.toLowerCase().replace(regEx, '');  // work with palindrome string 
   console.log("Forward: "+palindromeForward)
 
@@ -17,21 +21,31 @@ function IsPalindrome(palindrome) {
 
   } 
 
- 
 
-const App = () => {
-  const palindrome = 'saippuakauppias';
-    
+const App = (props) => {
+  const [palindrome, setPalindrome] = useState('');
+  const handleSubmit = () => setPalindrome(IsPalindrome({palindrome}));
+  
   return(
     <div>
-      <h1>Test Palindrome!</h1>
-      <p>Your test {palindrome} is <IsPalindrome palindrome = {palindrome} /></p>
+      <form onSubmit= {handleSubmit} >
+          <h1>Test Palindrome!</h1>
+          <label>
+            Palindrome to be examined:
+            <input 
+            name="palindrome" 
+            value={palindrome} 
+            onChange = {e => setPalindrome(e.target.value)}/>
+          </label>
+         {/* <button text="Submit" onChange={handleSubmit}>Submit</button> */}
+      </form>
+      <p>Your {palindrome} is <IsPalindrome palindrome = {palindrome} /></p>
     </div>
   )
 }
 export default App;
 
 ReactDOM.render(
-  <App/>,
+  <App />,
   document.getElementById('root')
 )

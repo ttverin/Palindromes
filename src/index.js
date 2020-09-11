6d43b44e862343a557226bf59dd770748ba9d1ec
ttverin <67685914+ttverin@users.noreply.github.com>
2020-09-11 09:26:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,19 +6,18 @@ import ReactDOM from 'react-dom'
 
 function IsPalindrome(palindrome) {
   console.log(palindrome.palindrome)
-  console.log({palindrome});
-  var regEx = new RegExp();  //remove non alphabetic characters and blanks ** add later
-  var palindromeForward = palindrome.palindrome.toLowerCase().replace(regEx, '');  // work with palindrome string 
+  var palindromeForward = palindrome.palindrome.toLowerCase();  // work with palindrome string 
+  palindromeForward = palindromeForward.replace('/[^A-Za-z0-9]/g', ''); //remove non alphabetic characters and blanks ** add later
+  console.log(palindrome.palindrome.toLowerCase().replace('/[^A-Za-z0-9]/g', ''));  
   console.log("Forward: "+palindromeForward)
 
-  var palindromeReverse = palindrome.palindrome.split('').reverse().join('');
+  var palindromeReverse = palindromeForward.split('').reverse().join('');
       //reverse generated string
 
 
   console.log("Reverse: " +palindromeReverse)
   console.log(palindrome.palindrome === palindromeReverse)
   return JSON.stringify(palindrome.palindrome === palindromeReverse);
-
   } 
 
 
@@ -39,7 +38,7 @@ const App = (props) => {
           </label>
          {/* <button text="Submit" onChange={handleSubmit}>Submit</button> */}
       </form>
-      <p>Your {palindrome} is <IsPalindrome palindrome = {palindrome} /></p>
+      <p>Your test case {palindrome} is <IsPalindrome palindrome = {palindrome} /></p>
     </div>
   )
 }
